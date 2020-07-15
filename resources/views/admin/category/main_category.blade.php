f0d5104915f524d042aa26a90f05289830b2519b
y-sekioka <y.sekioka84@gmail.com>
2020-07-16 00:29:13 +0900

    test

--- resources/views/admin/category/main_category.blade.php
+++ resources/views/admin/category/main_category.blade.php
@@ -20,14 +20,14 @@
                         </div>
                     </div>
                     <div class="form-group row">
-                        {{-- <div class="col-md-8">
+                        <div class="col-md-8">
                             <select class="form-contol" name="dictionary_id">
                                 <option value="">属する辞書を選択してください</option>
                                     @foreach($dictionary_posts as $dictionary_post)
                                         <option value="{{ $dictionary_post->id }}">{{ $dictionary_post->name }}</option>
                                     @endforeach
                             </select>
-                        </div> --}}
+                        </div>
                     </div>
                     <input type="submit" class="btn btn-primary" value="追加">
                 </form>
@@ -49,7 +49,7 @@
                             <tr>
                                 <td>{{ str_limit($post->id, 100) }}</td>
                                 <td>{{ str_limit($post->name,100) }}</td>
-{{--                                 <td>{{ str_limit($post->dictionary_id,100) }}</td> --}}
+                                <td>{{ str_limit($post->dictionary_id,100) }}</td>
                                 <td>
                                     <div>
                                         <a href="{{ action('Admin\CategoryController@main_category_delete', ['id' => $post->id]) }}">削除</a>
@@ -61,7 +61,7 @@
                 </table>
             </div>
         </div>
-       {{--  <div class="row">
+        <div class="row">
             <div class="col-md-8">
                 <table class="table table-dark">
                     <thead>
@@ -86,7 +86,7 @@
                     </tbody>
                 </table>
             </div>
-        </div> --}}
+        </div>
         <div category_link>
             <a href="{{ action('Admin\CategoryController@get_sub_category')}}">サブカテゴリへ</a>
         </div>
