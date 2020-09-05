7eb80f21da11f6b2332069c00787814dd5a6269c
y-sekioka <y.sekioka84@gmail.com>
2020-09-05 22:11:00 +0900

    mydic-beta2.2

--- resources/views/admin/top/home.blade.php
+++ resources/views/admin/top/home.blade.php
@@ -6,11 +6,20 @@
             <div class="row row-title">
                 <h1>辞書サイトトップページ</h1>
             </div>
-            @foreach ($posts as $post)
-            <div class="row row-big">
-                <a href="{{ action('Admin\WordController@dictionary_master',['id'=>$post->id,'name'=>$post->name]) }}">{{$post->name}}</a>
-            </div>
-            @endforeach
+            @if(count($posts) > 0))
+                @foreach ($posts as $post)
+                    <div class="row row-big">
+                        <a href="{{ action('Admin\WordController@dictionary_master',['id'=>$post->id,'name'=>$post->name]) }}">{{$post->name}}</a>
+                    </div>
+                @endforeach
+            @else
+                <h2>まだあなたの辞書が登録されてされていないようです！</h2>
+                <h3>下記の手順に従って辞書を登録してください。</h3>
+                <p>①<a href="{{ action('Admin\CategoryController@get_dictionary')}}">「辞書登録」</a>にて辞書を登録。</p>
+                <p>②<a href="{{ action('Admin\CategoryController@get_main_category')}}">「メインカテゴリ登録」</a>にて辞書に属するカテゴリを登録</p>
+                <p>③<a href="{{ action('Admin\CategoryController@get_sub_category')}}">「サブカテゴリ登録」</a>にて更に細かいカテゴリを登録してください。</p>
+                <p>①②③を終えると、「単語登録」より単語を辞書に登録する事ができます！</p>
+            @endif
         </div>
     </body>
 @endsection
