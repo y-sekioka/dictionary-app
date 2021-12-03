7e66f305d1ce80e51b65855afd134bc9aed87cb0
y-sekioka <y.sekioka84@gmail.com>
2021-12-03 14:54:03 +0900

    topページ誤字修正

--- resources/views/admin/top/home.blade.php
+++ resources/views/admin/top/home.blade.php
@@ -6,7 +6,7 @@
             <div class="row row-title">
                 <h1>辞書サイトトップページ</h1>
             </div>
-            @if(count($posts) > 0))
+            @if(count($posts) > 0)
                 @foreach ($posts as $post)
                     <div class="row row-big">
                         <a href="{{ action('Admin\WordController@dictionary_master',['id'=>$post->id,'name'=>$post->name]) }}">{{$post->name}}</a>
